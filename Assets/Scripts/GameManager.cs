b2ac1595b2f0965aa0a1e03ab10be8f682f71dc3
sev17238 <sevilla.diego8@gmail.com>
2019-10-06 21:58:59 -0600

    Seccion 6: Sistemas basicos lista
     - Se agrego skybox para la iluminacion del ambiente.
     - Ukron tiene las mecanicas mas basicas
     - BlackScreen funcionando

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private Vector3 respawnPosition;
+
+    public GameObject deathEffect;
+
+    public int currentCoins;
+
+    public int levelEndMusic = 8;
+
+    public string levelToLoad;
+
+    public bool isRespawning;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        respawnPosition = PlayerController.instance.transform.position;
+
+        //AddCoins(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //PAUSE MENU
+        /*if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseUnPause();
+        }*/
+    }
+
+    public void Respawn()
+    {
+        StartCoroutine(RespawnCo());
+        //HealthManager.instance.PlayerKilled();
+
+    }
+
+    /** A coroutine can be called wherever you want in your code, though the coroutine starts, 
+      * the code after its call keep going at its original order at the same time, in the same frame.
+      */
+    public IEnumerator RespawnCo()
+    {
+        PlayerController.instance.gameObject.SetActive(false);
+        CameraController.instance.theCMBrain.enabled = false;
+
+        UIManager.instance.fadeToBlack = true;
+        //Instantiate(deathEffect, PlayerController.instance.transform.position + new Vector3(0f, 1f, 0f), PlayerController.instance.transform.rotation);
+        yield return new WaitForSeconds(2f); //after two seconds the player is respawn to its original position.
+        isRespawning = true;
+        //HealthManager.instance.ResetHealth();
+        UIManager.instance.fadeFromBlack = true;
+
+        PlayerController.instance.transform.position = respawnPosition;
+        CameraController.instance.theCMBrain.enabled = true;
+
+        PlayerController.instance.gameObject.SetActive(true);
+    }
+
+
+    public void SetSpawnPoint(Vector3 newSpawnPoint)
+    {
+        respawnPosition = newSpawnPoint;
+        Debug.Log("SpawnPoint set");
+    }
+
+    /*public void AddCoins(int coinsToAdd)
+    {
+        currentCoins += coinsToAdd;
+        UIManager.instance.coinText.text = currentCoins.ToString();
+    }
+
+    public void PauseUnPause()
+    {
+        if (UIManager.instance.PauseScreen.activeInHierarchy)
+        {
+            UIManager.instance.PauseScreen.SetActive(false);
+            Time.timeScale = 1f;
+
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            UIManager.instance.PauseScreen.SetActive(true);
+            UIManager.instance.CloseOptions();
+            Time.timeScale = 0f;
+
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    public IEnumerator LevelEndCo()
+    {
+        AudioManager.instance.PlayMusic(levelEndMusic);
+        PlayerController.instance.stopMove = true;
+        UIManager.instance.fadeToBlack = true;
+
+        yield return new WaitForSeconds(4f);
+        Debug.Log("Level ended");
+
+        PlayerPrefs.SetInt(SceneManager.GetActiveScene().name + "_unlocked", 1);
+
+        //for coins
+        if (PlayerPrefs.HasKey(SceneManager.GetActiveScene().name + "_coins"))
+        {
+            if (currentCoins > PlayerPrefs.GetInt(SceneManager.GetActiveScene().name + "_coins"))
+            {
+                PlayerPrefs.SetInt(SceneManager.GetActiveScene().name + "_coins", currentCoins);
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetInt(SceneManager.GetActiveScene().name + "_coins", currentCoins);
+        }
+        //---------
+
+        SceneManager.LoadScene(levelToLoad);
+
+    }*/
+}
