c3a06be9eec26949b2fc0e4bea3963c3fd683309
Alejandro Tejada <tej17584@uvg.edu.gt>
2019-10-24 22:18:23 -0600

    partícula de union y level select

---
+++ Assets/Scripts/LevelSelect/LSLevelEntry.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LSLevelEntry : MonoBehaviour
+{
+    public string levelName, levelToCheck;
+    private bool canLoadLevel, levelUnlocked;
+
+    public GameObject mapPointActive, mapPointInactive;
+
+    private bool levelLoading;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.GetInt(levelToCheck + "_unlocked") == 1 || levelToCheck == "")
+        {
+            mapPointActive.SetActive(true);
+            mapPointInactive.SetActive(false);
+            levelUnlocked = true;
+        }
+        else
+        {
+            mapPointActive.SetActive(false);
+            mapPointInactive.SetActive(true);
+            levelUnlocked = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && canLoadLevel && levelUnlocked)
+        {
+            StartCoroutine(LevelLoadCo());
+            levelLoading = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canLoadLevel = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canLoadLevel = false;
+        }
+    }
+
+    public IEnumerator LevelLoadCo()
+    {
+        PlayerController.instance.stopMove = true;
+        UIManager.instance.fadeToBlack = true;
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(levelName);
+    }
+}
---
+++ Assets/Scripts/LevelSelect/LSResetPosition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LSResetPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerController.instance.gameObject.SetActive(false);
+            PlayerController.instance.transform.position = new Vector3(16.48f, 2, -3.12f);
+            PlayerController.instance.gameObject.SetActive(true);
+        }
+    }
+}
