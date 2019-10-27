298baf1ac819486d8602527e30dd913d597d9e15
Alejandro Tejada <tej17584@uvg.edu.gt>
2019-10-26 23:35:06 -0600

    Cambios Level Select

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    //Variables para el nivel
+    public string firstLevel, levelSelect;
+
+    public GameObject continueButton;
+
+    // Start is called before the first frame update
+    void Start () {
+        if (PlayerPrefs.HasKey ("Continue")) {
+            continueButton.SetActive (true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () { }
+
+    public void NewGame () {
+        SceneManager.LoadScene (firstLevel);
+        PlayerPrefs.SetInt ("Continue", 0);
+    }
+
+    public void Continue () {
+        SceneManager.LoadScene (levelSelect);
+    }
+
+    public void Quit () {
+        Application.Quit ();
+    }
+}
